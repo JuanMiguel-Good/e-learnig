eada1752430e56b71830b47c1987ba7e83b4fda8
JuanMiguel-Good <juan.miguel@goodsolutions.pe>
2025-11-07 18:14:32 -0500

    Remove problematic migrations and create empty test

---
+++ supabase/migrations/20251107231424_sweet_castle.sql
@@ -0,0 +1,9 @@
+/*
+  # Empty Test Migration
+  
+  This migration is COMPLETELY empty to test if there's something else
+  causing the bucket duplication error.
+*/
+
+-- This migration intentionally does nothing
+SELECT 1;
